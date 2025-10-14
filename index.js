6fcf9668757a14084117cadd614728398f4449c3
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-10-14 19:00:15 +0000

    check

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var distanceInMiles = 100;
+
+function estimatedArrivalTime = () {
+    let milesPerHour = 50;
+    return distanceInMiles / estimatedArrivalTime;
+}
+
+document.getElementById("result").innerHTML = estimatedArrivalTime
+
 
