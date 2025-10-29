4681de668e8a17e237804c63050bd255975c71ad
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-10-29 11:35:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var distanceInMiles = 100;
 
 let estimatedArrivalTime = function () {
     let milesPerHour = 50;
-    return distanceInMiles / estimatedArrivalTime;
+    return distanceInMiles / milesPerHour/;
 }
 
 document.getElementById("result").innerHTML = estimatedArrivalTime;
@@ -11,4 +11,5 @@ should create a global variable named distanceInMiles and set it to 100
 - should create a function called estimatedArrivalTime that has no parameters
 - should return the distanceInMiles divided by the milesPerHour from the estimatedArrivalTime function
 - should assign the innerHTML of the HTML element with the id result to return value from estimatedArrivalTime
+
 
